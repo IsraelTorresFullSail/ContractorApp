41a54df6eb6d25e8beab4f5e061bbc953194d278
Israel Torres <israel.a.torres01@gmail.com>
2019-06-13 18:17:21 -0400

    Making Punch List Functionality

--- src/app/components/punch-list/punch-list.component.ts
+++ src/app/components/punch-list/punch-list.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { MatSort, MatTableDataSource, MatPaginator } from '@angular/material';
 import { ListServiceService } from './../../services/list-service.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-punch-list',
   templateUrl: './punch-list.component.html',
-  styleUrls: ['./punch-list.component.css']
+  styleUrls: ['./punch-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class PunchListComponent implements OnInit {
 
-  constructor(private listService: ListServiceService, private router: Router) { }
+  constructor(private listService: ListServiceService, private router: Router) {}
 
-  displayedColumns: string[] = ['openingNumber', 'taskDescription', 'completed', 'actions'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   dataSource = new MatTableDataSource();
 
-  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = ['firstName', 'lastName'];
+  expandedElement = new MatTableDataSource();
+
+  isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
 
   ngOnInit() {
     this.getAllTasks();
